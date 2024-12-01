e084fe2a7de7b0db8c5d6ff428362edcc7839eea
Peter-eating <yitinglu071@gmail.com>
2024-12-02 03:22:39 +0800

    Update 📍_Marker_Cluster.py

--- pages/📍_Marker_Cluster.py
+++ pages/📍_Marker_Cluster.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-import pandas as pd
-import numpy as np
-import geopandas as gpd
 
+st.set_page_config(layout="wide")
 
 markdown = """
 A Streamlit map template
@@ -15,32 +13,24 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-
-st.title("Interactive Map")
-
-col1, col2 = st.columns([4, 1])
-options = list(leafmap.basemaps.keys())
-index = options.index("OpenTopoMap")
-with col2:
-
-    basemap = st.selectbox("Select a basemap:", options, index)
-
-TW_fishing=gpd.read_file('https://github.com/Peter-eating/W10homework/raw/refs/heads/main/KINMEN%20Air-Raid%20Shelter.csv')
+st.title("Marker Cluster")
 
 with st.expander("See source code"):
-    
     with st.echo():
-        with col1:
 
-            m = leafmap.Map(center=[23.7652,120.4980],zoom=8,
-            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
-            )
-            m.add_basemap(basemap)
-            m.add_points_from_xy(TW_fishing,
-                          x="經度",
-                          y="緯度",
-                          spin=True,
-                          add_legend=True,
-                          layer_name='全台開放釣點')
-            m.to_streamlit(height=700)
-st.dataframe(TW_fishing)
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+        regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+
+        m.add_geojson(regions, layer_name="US Regions")
+        m.add_points_from_xy(
+            cities,
+            x="longitude",
+            y="latitude",
+            color_column="region",
+            icon_names=["gear", "map", "leaf", "globe"],
+            spin=True,
+            add_legend=True,
+        )
+
+m.to_streamlit(height=700)
