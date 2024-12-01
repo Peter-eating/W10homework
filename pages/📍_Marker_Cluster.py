86294ee2a167de1e65151cade7f7ca3b050a0915
Peter-eating <yitinglu071@gmail.com>
2024-12-02 02:03:09 +0800

    Update 📍_Marker_Cluster.py

--- pages/📍_Marker_Cluster.py
+++ pages/📍_Marker_Cluster.py
@@ -19,18 +19,15 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[24.445033, 118.375672],zoom=8,
-            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
-            )
+            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
             m.add_basemap(basemap)
             cities = "https://github.com/Peter-eating/W10homework/raw/refs/heads/main/KINMEN%20Air-Raid%20Shelter.csv"
 
             m.add_points_from_xy(
-                cities,
-                x='緯度',
-                y='經度',
-                spin=True,
-                add_legend=True,
-                layer_name='金門防空避難點'
-            )
-
+                                    cities,
+                                    x='緯度',
+                                    y='經度',
+                                    spin=True,
+                                    add_legend=True,
+                                    layer_name='金門防空避難點')
             m.to_streamlit(height=700)
