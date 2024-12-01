e42dac4a5f289c1fa22837d832b621909fafa824
Peter-eating <yitinglu071@gmail.com>
2024-12-02 03:14:12 +0800

    Update 📍_Marker_Cluster.py

--- pages/📍_Marker_Cluster.py
+++ pages/📍_Marker_Cluster.py
@@ -15,17 +15,13 @@ with st.expander("See source code"):
 
         m = leafmap.Map(center=[24.441831, 118.353515], zoom=4)
         cities = "https://raw.githubusercontent.com/Peter-eating/W10homework/refs/heads/main/KINMEN%20Air-Raid%20Shelter.csv"
-        regions = "https://github.com/Peter-eating/W10homework/raw/refs/heads/main/KINMEN.shp"
 
         m.add_shp(regions, layer_name="KINMEN")
         m.add_points_from_xy(
             cities,
             x="經度",
             y="緯度",
-            color_column=" region",
-            icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
-            add_legend=True,
             
         )
 
