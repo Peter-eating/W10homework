87aa72d912f1da812928087ac64e56a6ebc1a9b1
Peter-eating <yitinglu071@gmail.com>
2024-12-02 02:09:46 +0800

    Update 📍_Marker_Cluster.py

--- pages/📍_Marker_Cluster.py
+++ pages/📍_Marker_Cluster.py
@@ -14,14 +14,14 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
-        cities = "https://github.com/8048-kh/test/raw/refs/heads/main/Aboriginal%20Tribes%20area.csv"
+        cities = "https://raw.githubusercontent.com/Peter-eating/W10homework/refs/heads/main/KINMEN%20Air-Raid%20Shelter.csv"
         regions = "https://github.com/8048-kh/test/raw/refs/heads/main/REGION.shp"
 
         m.add_shp(regions, layer_name="Aboriginal Tribes")
         m.add_points_from_xy(
             cities,
-            x="longitude",
-            y="latitude",
+            x="緯度",
+            y="經度",
             color_column="region",
             icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
