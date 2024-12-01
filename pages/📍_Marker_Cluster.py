2744d53d3bc47e143868b57706231dbc9e3f84c1
Peter-eating <yitinglu071@gmail.com>
2024-12-02 02:00:29 +0800

    Update 📍_Marker_Cluster.py

--- pages/📍_Marker_Cluster.py
+++ pages/📍_Marker_Cluster.py
@@ -18,18 +18,19 @@ st.title("Marker Cluster")
 with st.expander("See source code"):
     with st.echo():
 
-        m = leafmap.Map(center=[24.445033, 118.375672], zoom=4)
-        cities = "https://github.com/Peter-eating/W10homework/raw/refs/heads/main/KINMEN%20Air-Raid%20Shelter.csv"
-        regions = "https://github.com/8048-kh/test/raw/refs/heads/main/REGION.shp"
+        m = leafmap.Map(center=[24.445033, 118.375672],zoom=8,
+            locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
+            )
+            m.add_basemap(basemap)
+            cities = "https://github.com/Peter-eating/W10homework/raw/refs/heads/main/KINMEN%20Air-Raid%20Shelter.csv"
 
-        m.add_shp(regions, layer_name="Aboriginal Tribes")
-        m.add_points_from_xy(
-            cities,
-            x="緯度",
-            y="經度",
-            color_column="region",
-            icon_names=["gear", "map", "leaf", "globe"],
-            spin=True,
-        )
+            m.add_points_from_xy(
+                cities,
+                x='緯度',
+                y='經度',
+                spin=True,
+                add_legend=True,
+                layer_name='金門防空避難點'
+            )
 
-m.to_streamlit(height=700)
+            m.to_streamlit(height=700)
