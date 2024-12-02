0be0e8d165fd6649b92a20c8fa43d24bef9e090f
Peter-eating <yitinglu071@gmail.com>
2024-12-02 14:43:10 +0800

    Update 📍_Marker_Cluster.py

--- pages/📍_Marker_Cluster.py
+++ pages/📍_Marker_Cluster.py
@@ -13,26 +13,25 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-st.title("Marker Cluster")
+st.title("KINMEN Air-Raid Shelter")
 
 with st.expander("See source code"):
     with st.echo():
 
-        m = leafmap.Map(center=[40, -100], zoom=4)
-        cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+        m = leafmap.Map(center=[24.445748, 118.377391], zoom=4)
+        cities = "https://github.com/Peter-eating/hw10.2/raw/refs/heads/main/KINMEN%20Air-Raid%20Shelter%20(1).csv"
+        regions = "https://github.com/Peter-eating/hw10.2/raw/refs/heads/main/KINMEN.shp"
 
-        m.add_geojson(regions, layer_name="US Regions")
+        m.add_geojson(regions, layer_name="鄉鎮區界")
         m.add_points_from_xy(
             cities,
-            x="longitude",
-            y="latitude",
-            color_column="region",
-            icon_names=["gear", "map", "leaf", "globe"],
+            x="Longitude",
+            y="Latitude",
+            color_column="轄管分局",
+            icon_names=["gear", "map"],
             spin=True,
             add_legend=True,
         )
 
 m.to_streamlit(height=700)
-
-m.to_streamlit(height=700)
+st.dataframe(cities)
