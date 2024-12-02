402bd13462f21b4b32527c51d7bfa387ae6ac493
Peter-eating <yitinglu071@gmail.com>
2024-12-02 15:07:42 +0800

    Update csv.py

--- pages/csv.py
+++ pages/csv.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
 import pandas as pd
-import csv
+import requests
+from io import StringIO
 
 st.set_page_config(layout="wide")
 
@@ -17,5 +17,8 @@ st.sidebar.image(logo)
 
 st.title("KINMEN Air-Raid Shelter")
 
-cities = "https://github.com/Peter-eating/W10homework/raw/refs/heads/main/KINMEN%20Air-Raid%20Shelter.csv"
-st.dataframe(cities)
+csv_url = "https://github.com/yk-lin1021/113-1gis/raw/refs/heads/main/rain.csv"
+response = requests.get(csv_url)
+csv_data = response.text  
+df = pd.read_csv(StringIO(csv_data))
+st.dataframe(df)
